e97c0e752068221dadb0cfc46beba29acfd1ce1f
Pavle Lee <523260513@qq.com>
2019-03-14 15:54:40 +0800

    upgrade deploy log

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w -s' -mod=vendor -o tmp/deploy_linux_amd64
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags '-w -s' -mod=vendor -o tmp/deploy_windows_amd64
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -ldflags '-w -s' -mod=vendor -o tmp/deploy_mac_amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w -s' -mod=vendor -o build/deploy_linux_amd64
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags '-w -s' -mod=vendor -o build/deploy_windows_amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -ldflags '-w -s' -mod=vendor -o build/deploy_mac_amd64
